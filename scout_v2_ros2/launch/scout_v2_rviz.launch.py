899d2877ef7ba5657e5227b1920d897bcd284156
leonlime <leo.lima21051990@gmail.com>
2026-02-11 14:46:38 -0300

    add slam toolbox

--- scout_v2_ros2/launch/scout_v2_rviz.launch.py
+++ scout_v2_ros2/launch/scout_v2_rviz.launch.py
@@ -7,7 +7,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     scout_v2_ros2_path = get_package_share_directory('scout_v2_ros2')
     rviz_config_file = os.path.join(scout_v2_ros2_path,
-                                    'config/rviz',
+                                    'config',
                                     'config.rviz')
     print(scout_v2_ros2_path)
     print(rviz_config_file)
